de1b550a164010c26b8b090d21f1a3064e3bf3b6
Gerardo Quiñonez <gerardo01302011@gmail.com>
2020-07-30 18:17:33 -0600

    Agregando todo

---
+++ src/components/Ejemplo.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./StyleEjemplo.css";
+
+const Ejemplo = () => {
+	// Estado que usaremos para atrapar el valor del input
+	const [nombres, actualizarNombres] = useState("");
+
+	// URL para buscar
+	let API = `https://rickandmortyapi.com/api/character/?name=${nombres}`;
+
+	// Fetch para el Personaje
+	const [data, setData] = useState([]);
+
+	var obtenerDatos = async () => {
+		const datos = await fetch(API);
+		const personajes = await datos.json();
+
+		setData(personajes);
+	};
+
+	const handleChange = (e) => {
+		actualizarNombres(e.target.value);
+	};
+
+	const CambiarUrl = () => {
+		API = `https://rickandmortyapi.com/api/character/?page=1`;
+		console.log(API);
+	};
+
+	return (
+		<div className="Container">
+			<h4>Ingrese el nombre del personaje</h4>
+			<input name="personaje" onChange={handleChange} />
+
+			<button onClick={obtenerDatos}>Personajes</button>
+			<button onClick={CambiarUrl}>Cambiar</button>
+		</div>
+	);
+};
+
+export default Ejemplo;
